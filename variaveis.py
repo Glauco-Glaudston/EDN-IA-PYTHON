ffb5d53b426e212dc458405bd1259c98f1e318c2
Glaudiston Pereira <glaudiston@gmail.com>
2025-05-03 19:00:59 -0300

    Envio da atividade  Python

---
+++ variaveis.py
@@ -0,0 +1,11 @@
+nome = "Glaudston"
+_nome = "Glaudston"
+nome = 2025
+sobreome_glaudston = "Pereira"
+Nome = "Glaudston2"
+
+idade = 46
+altura = 1.84
+print(nome)
+print(type(idade))
+print(type(altura))
